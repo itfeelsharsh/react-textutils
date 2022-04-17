bcb89cc64850827274ef703faf296ed4c82ead81
Harsh <itfeelsharsh@gmail.com>
2022-04-17 11:27:10 +0530

    added TextForm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Navbar from "./components/Navbar.js";
+import TextForm from "./components/TextForm.js";
 
 function App() {
   return (
     <>
+
+
+
     <Navbar title="TextUtils" aboutText="About This App" />
+    
+   <div className = "container my-5" >
+   <TextForm heading="Enter the text you want to utilize" />
+   </div>
+
+
     </>
   );
 }
