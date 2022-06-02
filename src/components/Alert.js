01c0a70ddd111afc8fbc9595f3f6d3aded52c32a
itfeelsharsh <itfeelsharsh@gmail.com>
2022-06-02 21:08:45 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function Alert(props) {
+  const docap = (word) => {
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  };
 
-    const docap = (word) => {
-        const lower = word.toLowerCase();
-        return lower.charAt(0).toUpperCase() + lower.slice(1);
-    }  
-
-    return (
-        <div style={{height: "50px"}}>
-        {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-            <strong>{docap(props.alert.type)}</strong>: {props.alert.msg}
-        </div>} 
+  return (
+    <div style={{ height: "50px" }}>
+      {props.alert && (
+        <div
+          className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+          role="alert"
+        >
+          <strong>{docap(props.alert.type)}</strong>: {props.alert.msg}
         </div>
-  )
+      )}
+    </div>
+  );
 }
 
-export default Alert
+export default Alert;
