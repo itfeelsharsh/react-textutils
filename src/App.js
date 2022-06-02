c69a2b845f6aadbe65bf1c9c50ccfbbb499b3084
itfeelsharsh <itfeelsharsh@gmail.com>
2022-06-02 21:07:32 +0530

    code format

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import Navbar from "./components/Navbar.js";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import TextForm from "./components/TextForm.js";
 import Alert from "./components/Alert.js";
 import About from "./components/About.js";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-  //export default App;
-  //Data Flows from App.js to TextForm.js and Navbar.js to TextForm.js and Navbar.js to App.js.
-
+//export default App;
+//Data Flows from App.js to TextForm.js and Navbar.js to TextForm.js and Navbar.js to App.js.
 
 function App() {
-
   const [mode, setMode] = useState("light");
 
   const [alert, setAlert] = useState(null);
@@ -22,7 +16,7 @@ function App() {
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
+      type: type,
     });
     setTimeout(() => {
       setAlert(null);
@@ -30,45 +24,47 @@ function App() {
   };
 
   const toggleMode = () => {
-    if(mode === 'light'){
-      setMode('dark');
+    if (mode === "light") {
+      setMode("dark");
       document.body.style.backgroundColor = "#2C2F33";
       showAlert("Dark mode enabled", "success");
-      
-    }
-    else{
-      setMode('light');
+    } else {
+      setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode enabled", "success");
-    } 
-  }
+    }
+  };
   return (
+    <>
+      <Navbar
+        title="TextUtils"
+        aboutText="About This App"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
 
-<> 
+      <Alert alert={alert} />
 
-    <Navbar title="TextUtils" aboutText="About This App" mode={mode} toggleMode={toggleMode} />
-
-
-    <Alert alert={alert} />
-    
-   <div className = "container my-5" >
-
-    <BrowserRouter>
-
-      <Routes>
-              <Route path="/" element={<TextForm showAlert={showAlert} heading="Edit your text below" toggleMode={toggleMode} mode={mode} />} />
-              <Route path="/about" element={<About />} mode={mode} />
-      </Routes>
-
-    </BrowserRouter>
-     
-
-   </div>
-
-
-   </>  
-           
+      <div className="container my-5">
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Edit your text below"
+                  toggleMode={toggleMode}
+                  mode={mode}
+                />
+              }
+            />
+            <Route path="/about" element={<About />} mode={mode} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
-}         //end of App
+} //end of App
 
 export default App;
